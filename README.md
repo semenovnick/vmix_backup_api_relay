8df06a7e90c0d40cee1fd61e9c9d5c5d48d1b03c
semenovnick <semenov_nick@mail.ru>
2024-02-06 17:11:48 +0300

    ver 0.9

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # vmix_backup_api_relay
 
 Relay app for VMIX API
-This app will use **_"Main"_** and **_"Backup"_** VMIX instances. The application analyzes the connection status to the **_Main_** and **_Backup_** VMIX instance. Depending on the state, switches the output of the xml request result from `http://vmixip:vmixport/api`. Thus, simulating one VMIX instance. Requests via `/api?Function=` are duplicated on both **_Main_** and **_Backup_**
+This app will use **_"Main"_** and **_"Backup"_** VMIX instances. The application switches the connection to the **_Main_** or **_Backup_** VMIX instance.
+App API:
+
+- /set with parameters:
+  - Backup=true - if you want to change **_Backup_** parameters, left empty if you want to change **_Main_**
+  - ip
+  - port
+- /switch with parameters:
+
+  - to=backup or to=main - choose what instance you want to switch
+
+- /getStatus - returning which instance is selected
+- /getConfig - returning config
--- src/app.js
+++ src/app.js
@@ -4,12 +4,86 @@ const configFilePath = './config/config.json';
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync(configFilePath));
 const port = config.port;
-console.log(config);
+const url = require('url');
+// console.log(config);
+
+let VMIXip = config.mainVmix.ip;
+let VMIXport = config.mainVmix.port;
 
 app.get('/api', (req, res) => {
-  res.send('Hello World!');
+  const urlObject = {
+    protocol: 'http',
+    hostname: VMIXip,
+    port: VMIXport,
+    pathname: '/api',
+    query: req.query,
+  };
+
+  res.redirect(url.format(urlObject));
 });
 
+app.get('/set', (req, res) => {
+  let vmixSettings = config.mainVmix;
+
+  if (req.query.backup != undefined) {
+    if (req.query.backup == true) {
+      vmixSettings = config.backupVmix;
+    }
+  }
+  if (req.query.ip != undefined) {
+    if (req.query.port != undefined) {
+      vmixSettings.ip = req.query.ip;
+      vmixSettings.port = req.query.port;
+    }
+  } else {
+    VMIXip = vmixSettings.ip;
+    VMIXport = vmixSettings.port;
+  }
+  if (req.query.backup != undefined) {
+    if (req.query.backup == true) {
+      config.backupVmix = vmixSettings;
+    }
+  } else {
+    config.mainVmix = vmixSettings;
+  }
+  fs.writeFileSync(configFilePath, JSON.stringify(config));
+  res.send({ config, status: 'Saved' });
+});
+
+app.get('/switch', (req, res) => {
+  let vmixSettings = config.mainVmix;
+  let status = 'Wrong Parameters';
+
+  if (req.query.to != undefined) {
+    if (req.query.to.toLowerCase() == 'backup') {
+      vmixSettings = config.backupVmix;
+      status = 'Switched to backup';
+    } else if (req.query.to.toLowerCase() == 'main') {
+      vmixSettings = config.mainVmix;
+      status = 'Switched to main';
+    }
+  }
+  VMIXip = vmixSettings.ip;
+  VMIXport = vmixSettings.port;
+  res.send({ status: status });
+});
+
+app.get('/getStatus', (req, res) => {
+  let status = 'Something went wrong';
+  if (VMIXip == config.mainVmix.ip && VMIXport == config.mainVmix.port) {
+    status = 'Main';
+  } else if (
+    VMIXip == config.backupVmix.ip &&
+    VMIXport == config.backupVmix.port
+  ) {
+    status = 'Backup';
+  }
+  res.send({ status: status });
+});
+
+app.get('/getConfig', (req, res) => {
+  res.send(config);
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
